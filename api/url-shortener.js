b3653f6d4ffe58eba4a46e8e50e45e55f03b416d
Rustin Alexandru <rustinalexandru@gmail.com>
2017-06-10 10:50:30 +0000

    Fix2 previous return error bug

--- api/url-shortener.js
+++ api/url-shortener.js
@@ -11,7 +11,7 @@ module.exports = function(app, db) {
                 .then(putUrlInDb, fail => {console.log(fail); throw new Error("Error url not valid")})
                 .then(resultedObj => res.send(resultedObj))
                 .then(() => db.close())
-                    .catch(err => {console.log("reason for fail \n" + err); res.send(err);}));
+                    .catch(err => {console.log("reason for fail \n" + err); res.send(err.toString());}));
     }
 
     function handleGetURL(req, res) {
